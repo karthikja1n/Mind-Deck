e1f2d10736ad5556a3a7893d67513a6882dff9cc
karthikja1n <karthikjain2007@gmail.com>
2025-05-24 13:43:24 +0530

    Delete checkers directory

--- checkers/build/testgoogle.js
+++
@@ -1,7 +0,0 @@
-
-const Compute = require('@google-cloud/compute');
-
-const compute = new Compute()
-
-const zone  = compute.zone('eu-west4-a');
-const name = '';
--- checkers/build/utils/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './css-utils'
-export {default as _env} from './env-utils'
-export * from './plugins'
-export * from './utils'
